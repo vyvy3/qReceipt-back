1b5425568118270cc489e74fe89ad58c45095911
Leonards Tesnovs <Leonards.Tesnovs@swisscom.com>
2023-01-26 01:25:06 +0200

    update

--- src/main/java/xyz/qakashi/qreceipt/repository/ReceiptFormRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.qakashi.qreceipt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import xyz.qakashi.qreceipt.domain.ReceiptForm;
-
-@Repository
-public interface ReceiptFormRepository extends JpaRepository<ReceiptForm, Long> {
-}
--- src/main/resources/db/migration/postgresql/Q2__Fix_ids.sql
+++
@@ -1,3 +0,0 @@
-select setval('qreceipt_user_id_seq', max(id)) from qreceipt_user;
-select setval('qreceipt_email_verification_id_seq', max(id)) from qreceipt_email_verification;
-select setval('qreceipt_role_id_seq', max(id)) from qreceipt_role;
