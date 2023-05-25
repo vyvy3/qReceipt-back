643b6441200dbd5944013c84f0122289f3d9b888
qchimera <reddit404error@gmail.com>
2023-05-25 22:47:05 +0600

    Fixed files storage location

--- src/main/java/xyz/qakashi/qreceipt/util/Constants.java
+++ src/main/java/xyz/qakashi/qreceipt/util/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final String PRIVATE_API_ENDPOINT = "/private";
     public static final String ADMIN_API_ENDPOINT = "/admin";
     public static final String API_USER_API_ENDPOINT = "/api";
-    public static final String FILE_UPLOAD_FOLDER = "\\files\\";
+    public static final String FILE_UPLOAD_FOLDER = "/files/";
 
     public static Integer CODE_LIFETIME_IN_SECONDS = 300;
     public static Integer CODE_BLOCK_TIME_IN_MINUTES = 5;
